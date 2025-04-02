202d645d2119a646dcc592d1e71ab0f4d6ef754c
MisTerio497 <ipkovalenko2006@gmail.com>
2025-04-02 15:02:11 +0300

    skibidi dop dop es es

--- php/tanks.php
+++ php/tanks.php
@@ -17,7 +17,7 @@ if (!$id) {
         <header>
             <nav class="searchPage">
                 <a href="/" class="header_link">Главная</a>
-                <a href="/tank" class="header_link">Танки</a>
+                <a href="/tanks-vs" class="header_link">Танки</a>
                 <a href="/search" class="header_link" id="line2">Поиск</a>
             </nav>
         </header>
@@ -47,7 +47,7 @@ if (!$tank) {
         <header>
             <nav class="searchPage">
                 <a href="/" class="header_link">Главная</a>
-                <a href="/tank" class="header_link">Танки</a>
+                <a href="/tanks-vs" class="header_link">Танки</a>
                 <a href="/search" class="header_link" id="line2">Поиск</a>
             </nav>
         </header>
@@ -72,7 +72,7 @@ if (!$tank) {
 <header>
     <nav class="searchPage">
         <a href="/" class="header_link">Главная</a>
-        <a href="/tank" class="header_link">Танки</a>
+        <a href="/tanks-vs" class="header_link">Танки</a>
         <a href="/search" class="header_link" id="line2">Поиск</a>
     </nav>
 </header>
