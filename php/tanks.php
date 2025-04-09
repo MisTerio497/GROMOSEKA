67fadf70225eee72f392633c26219fa5c5bcae52
MisTerio497 <ipkovalenko2006@gmail.com>
2025-04-09 12:56:40 +0300

    Update tanks.php

--- php/tanks.php
+++ php/tanks.php
@@ -63,7 +63,7 @@ if (!$tank) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($tank['nametank']) ?></title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/style.css?v=1.0.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Anonymous+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
